eec0456d8617e0a1545252c86d3805f53cee8f71
Isrothy <61075605+Isrothy@users.noreply.github.com>
2020-12-22 12:02:03 +0800

    Update palindromic_automation.h

--- string/palindromic_automation.h
+++ string/palindromic_automation.h
@@ -1,25 +1,29 @@
-struct Palindromic_Automation {
-    int Next[M][C], len[M], fail[M];
+struct palindromic_automaton {
+    int Next[M][C], fail[M], len[M], cnt[M];
     int tot;
 
+    int get_fail(char *S, int i, int p) {
+        while (i - len[p] - 1 == 0 || S[i - len[p] - 1] != S[i]) {
+            p = fail[p];
+        }
+        return p;
+    }
+
     void build(char *S, int n) {
-        fail[0] = tot = 1;
         len[1] = -1;
-        int p = 1;
+        fail[0] = 1;
+        tot = 1;
+        int p = 0;
         for (int i = 1; i <= n; ++i) {
-            while (S[i - len[p] - 1] != S[i])
-                p = fail[p];
-            int &q = Next[p][S[i] - 'a'];
-            if (!q) {
+            int q = get_fail(S, i, p);
+            if (Next[q][S[i] - 'a'] == 0) {
                 int r = ++tot;
-                len[r] = len[p] + 2;
-                p = fail[p];
-                while (S[i - len[p] - 1] != S[i])
-                    p = fail[p];
-                fail[r] = Next[p][S[i] - 'a'];
-                q = r;
+                len[r] = len[q] + 2;
+                fail[r] = Next[get_fail(S, i, fail[q])][S[i] - 'a'];
+                Next[q][S[i] - 'a'] = r;
             }
-            p = q;
+            p = Next[q][S[i] - 'a'];
         }
     }
+    
 };
