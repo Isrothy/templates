9813c55985052f9f9d1c1fdb782b33f227dcf796
Isrothy <61075605+Isrothy@users.noreply.github.com>
2020-12-22 17:58:33 +0800

    Create minimal_representation.h

---
+++ string/minimal_representation.h
@@ -0,0 +1,19 @@
+int minimal_representation(char *S, int n) {
+    int i = 0, j = 1, k = 0;
+    while (i < n && j < n && k < n) {
+        if (S[(i + k) % n] == S[(j + k) % n]) {
+            ++k;
+        } else {
+            if (S[(i + k) % n] < S[(j + k) % n]) {
+                j += k + 1;
+            } else {
+                i += k + 1;
+            }
+            if (i == j){
+                ++i;
+            }
+            k = 0;
+        }
+    }
+    return min(i, j);
+}
