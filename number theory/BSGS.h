f89cebe763b36a76251d22f8e027760d4cdf8a4d
Isrothy <61075605+Isrothy@users.noreply.github.com>
2020-12-24 13:21:23 +0800

    Update BSGS.h

--- number theory/BSGS.h
+++ number theory/BSGS.h
@@ -1,6 +1,6 @@
-int BSGS(long long a, long long b,int mod) {
-    if (a == 0)
-        return 1;
+int BSGS(long long a, long long b, int mod) {
+    if (mod == 1)
+        return 0;
     unordered_map<long long, int> Mp;
     long long w = 1, x = 1;
     int S = sqrt(mod) + 1;
@@ -9,9 +9,10 @@ int BSGS(long long a, long long b,int mod) {
         w = w * a % mod;
         Mp[b] = k;
     }
-    for (int k = 1;; ++k) {
+    for (int k = 1; k <= S; ++k) {
         x = x * w % mod;
         if (Mp.count(x) != 0)
-            return k * S - Mp[x];
+            return (k * S - Mp[x]) % (mod - 1);
     }
+    return -1;
 }
