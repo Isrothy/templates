2bcd11bbf84ecab0eb5096dec61d5c92151be003
Isrothy <61075605+Isrothy@users.noreply.github.com>
2020-12-24 21:18:56 +0800

    Create DFS.h

---
+++ graph theory/Two_SAT/DFS.h
@@ -0,0 +1,43 @@
+namespace Two_SAT {
+    vector<int> E[2 * M];
+    int stk[M];
+    bool mark[2 * M];
+    int top;
+
+    void add_clause(int u, bool f1, int v, bool f2) {
+        u = u << 1 | f1;
+        v = v << 1 | f2;
+        E[u ^ 1].push_back(v);
+        E[v ^ 1].push_back(u);
+    }
+
+    bool dfs(int x) {
+        if (mark[x ^ 1])
+            return false;
+        if (mark[x])
+            return true;
+        mark[x] = true;
+        stk[top++] = x;
+        for (int i = 0; i < (int) E[x].size(); ++i) {
+            if (!dfs(E[x][i]))
+                return false;
+        }
+        return true;
+    }
+
+    bool check(int n) {
+        for (int i = 0; i < 2 * n; i += 2) {
+            if (!mark[i] && !mark[i ^ 1]) {
+                top = 0;
+                if (!dfs(i)) {
+                    while (top != 0) {
+                        mark[stk[--top]] = false;
+                    }
+                    if (!dfs(i ^ 1))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
