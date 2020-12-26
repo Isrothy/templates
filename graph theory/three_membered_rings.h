986000be7b5199a0eaa035f4a5d4a1251977e3ee
Isrothy <61075605+Isrothy@users.noreply.github.com>
2020-12-26 12:41:12 +0800

    Create three_membered_rings.h

---
+++ graph theory/three_membered_rings.h
@@ -0,0 +1,29 @@
+int three_membered_rings(vector<int> *E, int n) {
+    static long long Rank[M];
+    static int vis_time[M];
+    static vector<int> F[M];
+    for (int u = 1; u <= n; ++u) {
+        Rank[u] = (long long) E[u].size() * (n + 1) + u;
+        vis_time[u] = 0;
+        F[u].clear();
+    }
+    for (int u = 1; u <= n; ++u) {
+        for (auto v : E[u]) {
+            if (Rank[v] < Rank[u])
+                F[u].push_back(v);
+        }
+    }
+    int res = 0;
+    for (int u = 1; u <= n; ++u) {
+        for (auto v : F[u]) {
+            vis_time[v] = u;
+        }
+        for (auto v : F[u]) {
+            for (auto w : F[v]) {
+                if (vis_time[w] == u)
+                    ++res;
+            }
+        }
+    }
+    return res;
+}
