7076b642a4d4f04e90a3e6f4787503a0d5e8af03
Isrothy <61075605+Isrothy@users.noreply.github.com>
2021-02-06 12:29:22 +0800

    Update homogeneous_linear_recursion_with_constant_coefficients.h

--- linear algebra/homogeneous_linear_recursion_with_constant_coefficients.h
+++ linear algebra/homogeneous_linear_recursion_with_constant_coefficients.h
@@ -1,5 +1,5 @@
 int homogeneous_linear_recursion_with_constant_coefficients(int *A, int *f, int k, int n) {
-    static int a[M], b[M], c[M], d[M];
+    static int a[M], b[M], c[M];
     for (int i = 0; i < k; ++i) {
         a[i] = -f[k - i];
         b[i] = c[i] = 0;
@@ -8,10 +8,10 @@ int homogeneous_linear_recursion_with_constant_coefficients(int *A, int *f, int 
     while (n != 0) {
         if ((n & 1) == 1) {
             multiply(b, c, c, k, k);
-            modular(c, a, d, c, 2 * k - 1, k + 1);
+            modular(c, a, c, 2 * k - 1, k + 1);
         }
         multiply(b, b, b, k, k);
-        modular(b, a, d, b, 2 * k - 1, k + 1);
+        modular(b, a, b, 2 * k - 1, k + 1);
         n >>= 1;
     }
     long long res = 0;
