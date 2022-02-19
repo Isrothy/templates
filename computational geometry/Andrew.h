f1400d309df518c7e5268ea5fa68b428bce2c93b
Jiang, Jiahe <61075605+Isrothy@users.noreply.github.com>
2022-02-19 21:54:18 +0800

    Update Andrew.h

--- computational geometry/Andrew.h
+++ computational geometry/Andrew.h
@@ -1,34 +1,22 @@
 int Andrew(Point *a, int n) {
     static Point b[M];
-    static int stk[M];
-    static bool used[M];
-    for (int i = 0; i < n; ++i) {
-        b[i] = a[i];
-        used[i] = false;
-    }
-    sort(b, b + n, [](Point const &A, Point const &B) {
+    sort(a, a + n, [](Point const &A, Point const &B) {
         return A.x == B.x ? A.y < B.y : A.x < B.x;
     });
     int top = 0;
-    stk[top++] = 0;
+    b[top++] = a[0];
     for (int i = 1; i < n; ++i) {
-        while (2 <= top && det(b[stk[top - 1]] - b[stk[top - 2]], b[i] - b[stk[top - 1]]) <= 0) {
-            used[stk[--top]] = false;
-        }
-        stk[top++] = i;
-        used[i] = true;
+        while (2 <= top && det(b[top - 1] - b[top - 2], a[i] - b[top - 1]) <= 0)
+            --top;
+        b[top++] = a[i];
     }
     int tmp = top;
     for (int i = n - 2; 0 <= i; --i) {
-        if (used[i])
-            continue;
-        while (tmp < top && det(b[stk[top - 1]] - b[stk[top - 2]], b[i] - b[stk[top - 1]]) <= 0) {
-            used[stk[--top]] = false;
-        }
-        stk[top++] = i;
-        used[i] = true;
+        while (tmp < top && det(b[top - 1] - b[top - 2], a[i] - b[top - 1]) <= 0)
+            --top;
+        b[top++] = a[i];
     }
     for (int i = 0; i < top; ++i)
-        a[i] = b[stk[i]];
+        a[i] = b[i];
     return top - 1;
 }
